e9f4d8d88fd236ef253fdd1d1a3e20f0958d1a45
3laa-boss <3laa.alawneh@gmail.com>
2024-03-09 02:02:48 +0200

    Update Vehicle.h

--- Vehicle.h
+++ Vehicle.h
@@ -1,14 +1,10 @@
-//
-// Created by Amroh on 3/9/2024.
-//
+
 #include <iostream>
 using namespace std;
 #ifndef DATA_HOME_VEHICLE_H
 #define DATA_HOME_VEHICLE_H
 
-struct Date{
-	Date operator=(char*a){}
-};
+
 class Vehicle {
 private :
 	Date ManufatureDate;
@@ -26,10 +22,28 @@ public :
 	Vehicle() { Brand = "Not initialized"; ManufatureDate = "1/1/2000"; };
 	~Vehicle() { delete(Brand); };
 	friend ostream& operator<<(ostream&out,Vehicle obj){
-		printf("%s\t", obj.Brand);
-		cout << obj.ManufatureDate;
+		printf("Brand:%s", obj.Brand);
+		cout <<", Manufaturing Date: " << obj.ManufatureDate;
+	}
+	bool operator==(Vehicle obj) {
+		if (this->Brand == obj.Brand && this->ManufatureDate == obj.ManufatureDate)return 1;
+		return 0;
 	}
 
+	Vehicle operator++() {
+		this->ManufatureDate.year++;
+		return *this;
+	}
+	void PrintAsVehicle() {
+		//because we cannot call the overload of << in the derived classes directly
+		cout << *this;
+	};
+	bool CheckIfEqualAsVehicle(Vehicle obj) {
+		//same with the print .. we cannot check the values in overloading car/motorcycle == operator
+		//nor can we use the overload we have in this ,, we leffy bena ya donya
+		if (*this == obj)return 1;
+		return 0;
+	}
 };
 
 
